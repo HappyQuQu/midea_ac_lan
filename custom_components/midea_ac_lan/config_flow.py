d6eb1a7159a85245b20b45d88c51d370ec44b8a4
EvanQu <EvanQu@outlook.com>
2024-03-30 17:12:14 +0800

    修复异常

--- custom_components/midea_ac_lan/config_flow.py
+++ custom_components/midea_ac_lan/config_flow.py
@@ -162,7 +162,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema({
                 vol.Required(CONF_ACCOUNT): str,
                 vol.Required(CONF_PASSWORD): str,
-                vol.Required(CONF_SERVER, default=1): vol.In(SERVERS)
+                vol.Required(CONF_SERVER, default=2): vol.In(SERVERS)
             }),
             errors={"base": error} if error else None
         )
